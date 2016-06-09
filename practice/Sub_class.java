7e6ad8fdb9a77f0509fac15349c2978ef51b88cd
skepticshivam <shivam.sharma9530@googlemail.com>
2016-06-09 16:41:21 +0530

    Super keyword

---
+++ practice/Sub_class.java
@@ -0,0 +1,44 @@
+class Super_class{
+
+   int num = 20;
+   
+   
+   public void display(){   
+      System.out.println("This is the display method of superclass");
+   }	
+
+}
+
+public class Sub_class extends Super_class {
+
+   int num = 10;
+   
+   
+   public void display(){
+      System.out.println("This is the display method of subclass");
+   }
+   
+   public void my_method(){
+	  
+      
+      Sub_class sub = new Sub_class();
+	  
+      
+      sub.display();
+	  
+      
+      super.display();
+	  
+      
+      System.out.println("value of the variable named num in sub class:"+ sub.num);
+		  
+     
+      System.out.println("value of the variable named num in super class:"+ super.num);     
+   }
+   
+   public static void main(String args[]){
+      Sub_class obj = new Sub_class();
+     obj.my_method();
+      
+   }
+}
